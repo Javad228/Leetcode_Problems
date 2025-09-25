243ab31bf983911f04506348e732f991517dd64b
Javad228 <javadbaghirov1@gmail.com>
2025-09-25 02:18:46 -0400

    Add solution for problem Rec-104: Implement maxDepth function for binary tree

---
+++ problem_rec_104.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution(object):
+    def maxDepth(self, root):
+        if not root:
+            return 0
+        left_depth = self.maxDepth(root.left)
+        right_depth = self.maxDepth(root.right)
+        return max(left_depth,right_depth)+1
+
+# Build the tree for input [3,9,20,null,null,15,7]
+root = TreeNode(3)
+root.left = TreeNode(9)
+root.right = TreeNode(20)
+root.right.left = TreeNode(15)
+root.right.right = TreeNode(7)
+
+# Run solution
+sol = Solution()
+print(sol.maxDepth(root))  # Expected output: 3
