1ee9ca86d808d0caea6b1ac717c53d98d9d8ac23
Javad228 <javadbaghirov1@gmail.com>
2025-10-07 00:45:33 -0400

    Add solution for problem MS-1227

---
+++ problem_ms_1227.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def nthPersonGetsNthSeat(self, n):
+        """
+        :type n: int
+        :rtype: float
+        """
+        if n ==1:
+            return 1
+        else:
+            return 0.5
+
+
+sol = Solution()
+sol.nthPersonGetsNthSeat(4)
